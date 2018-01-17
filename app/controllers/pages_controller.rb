77446ae17d1f46ecee61b074af9df24af43cadc1
ivan-fedotov <ivan.fedotov86@gmail.com>
2018-01-17 08:39:55 +0500

    network_name correction allowed

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -4,7 +4,7 @@ class PagesController < ApplicationController
 
   def reload
     if current_user.is_root
-      @msg = system("git pull o2o master")
+      @msg = system('git pull o2o master')
     end
   end
 end
